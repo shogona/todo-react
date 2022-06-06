4e81f749c53bb285b2cf569c020e7b8899bd2e38
Shogo Nakamura <dmx42ranz@gmail.com>
2022-06-06 21:42:10 +0900

    feat: create html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom";
 
-import App from "./App";
+import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
