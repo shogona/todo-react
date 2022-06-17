1768546f779ce27118d100c3e5de0fd90ffcdd69
Shogo Nakamura <dmx42ranz@gmail.com>
2022-06-17 21:24:41 +0900

    fix: component化

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
+export const InputTodo = (props) => {
+  const { todoText, onChangeTodoText, onClickAdd } = props;
+  return (
+    <div style={style}>
+      <input
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChangeTodoText}
+      />
+      <button onClick={onClickAdd}>追加</button>
+    </div>
+  );
+};
